dde97f459f3094341620f451bf4ae39728920d2b
Abdul <abdulmusty03@outlook.com>
2025-10-06 23:19:49 +0100

    chore: project setup

---
+++ app/fonts.ts
@@ -0,0 +1,56 @@
+import { Inter, Luxurious_Script, Poppins } from "next/font/google";
+import localFont from "next/font/local";
+
+export const aeonikTrial = localFont({
+    src: [
+        {
+            path: "../public/fonts/aeoniktrial-light.otf",
+            weight: "300",
+            style: "normal",
+        },
+        {
+            path: "../public/fonts/aeoniktrial-lightitalic.otf",
+            weight: "300",
+            style: "italic",
+        },
+        {
+            path: "../public/fonts/aeoniktrial-regular.otf",
+            weight: "400",
+            style: "normal",
+        },
+        {
+            path: "../public/fonts/aeoniktrial-regularitalic.otf",
+            weight: "400",
+            style: "italic",
+        },
+        {
+            path: "../public/fonts/aeoniktrial-bold.otf",
+            weight: "700",
+            style: "normal",
+        },
+        {
+            path: "../public/fonts/aeoniktrial-bolditalic.otf",
+            weight: "700",
+            style: "italic",
+        },
+    ],
+    display: "swap",
+    variable: "--font-aeonik-trial",
+});
+
+export const inter = Inter({
+    subsets: ["latin"],
+    variable: "--font-inter",
+});
+
+export const luxuriousScript = Luxurious_Script({
+    subsets: ["latin"],
+    weight: "400",
+    variable: "--font-luxurious-script",
+});
+
+export const poppins = Poppins({
+    subsets: ["latin"],
+    weight: ["400", "500", "600"],
+    variable: "--font-poppins",
+});
---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import { cn } from "@/lib/utils";
+import type { Metadata } from "next";
+import { aeonikTrial, inter, luxuriousScript, poppins } from "./fonts";
+import "./globals.css";
+
+export const metadata: Metadata = {
+    title: "Axel Promotions",
+    description: "Accelerate Growth with Proven Marketing & Sales Execution",
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en">
+            <body
+                className={cn(
+                    aeonikTrial.variable,
+                    inter.variable,
+                    luxuriousScript.variable,
+                    poppins.variable,
+                )}
+            >
+                {children}
+            </body>
+        </html>
+    );
+}
